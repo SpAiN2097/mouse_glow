48070167e6528829b8912eb545f0ad1ebea2c00e
JahilMag <jahil@magneticcreative.co.za>
2017-09-06 12:52:13 +0200

    Deployed

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# mouse_glow
+# Mouse Glow Halo effect
+
+> This is a very simple pure javascript plugin which enbles a halo effect round your mouse for the selected container.
+
+To get up and running, all you have to do is the good old `npm i` or just `yarn` it
+In your project you then need to import `mouseGlow` using the following method:
+`import mouseGlow from 'mouse_glow'`
+
+Usage
+```
+import mouseGlow from 'mouse_glow'
+mouse_glow(element,color)
+```
+
+mouse_glow accepts two parameters, both of type `string` where the first is your selector (pass selector css style `.class or #id`)
+The second parameter is the original background color of the container where you want to apply the effect.
+
+Enjoy and star if you like.
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,22 @@
+const mouseGlow = (element) => {
+  let lightColor = 'fff';
+  let gradientSize = 5;
+  const el = document.querySelector(element)   
+  let background = 'lightslategray' 
+  el.addEventListener('mousemove', (e) => {
+  let  x  = e.pageX - e.target.offsetLeft;
+  let  y  = e.pageY - e.target.offsetTop;
+  let  xy = x + " " + y;
+  let bgWebKit = "-webkit-gradient(radial, " + xy + ", 0, " + xy + ", 100, from(rgba(255,255,255,0.8)), to(rgba(255,255,255,0.0))), " + background;
+  let bgMoz    = "-moz-radial-gradient(" + x + "px " + y + "px 45deg, circle, " + lightColor + " 0%, " + background + " " + gradientSize + "px)";
+
+  el.style.background = bgWebKit
+  el.style.background = bgMoz
+
+})
+  el.addEventListener('mouseleave', (e) => {
+      e.target.style.background = background
+  })
+}
+
+export default mouseGlow
